0ce1c24afafbffee6ac628a70df1abbaeb05bb72
Alan Ono <alan2ono@gmailcom>
2017-03-29 21:36:51 -0300

    push notification config

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  push: {
+    android: {
+      senderId: '758094525244',
+      apiKey: 'AAAAsIH0czw:APA91bGg7yP_SvHLydoQmpL95DXaQKjSX_t_K4LZM7vNAY5rDue86_ZTZbMwm9zZxOBgAioaRwjHgYwJA6R9VNVHrqiwQgpjch3tJa9OKi403nCCK9ZrTCYlOi2AsQixLEU8B0hTsWbL'
+    }
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
